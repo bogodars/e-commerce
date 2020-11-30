a4c9d22648ebbb53c200cbf360ebe8552a3cb6ca
Bogodar Shmyglyk <bogodar@gmail.com>
2020-11-30 03:32:15 +0200

    mock edits

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {
   Card,
   CardMedia,
+  CardContent,
   CardActions,
   Typography,
   IconButton,
 } from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
 const Product = ({ product }) => {
   const classes = useStyles();
+
   return (
     <div>
       <Card className={classes.root}>
@@ -23,7 +25,7 @@ const Product = ({ product }) => {
             </Typography>
             <Typography variant='h5'>{product.price}</Typography>
           </div>
-          <Typography variant='h2' color='textSecondary'>
+          <Typography variant='body2' color='textSecondary'>
             {product.description}
           </Typography>
         </CardContent>
